c81ddc1519d8b81f75d8eb13ccf8c19f350b2dbd
yey <yosarseeff@gmail.com>
2022-08-03 00:48:57 +0100

    routes y views

---
+++ resources/views/vista1.blade.php
@@ -0,0 +1,8 @@
+<html>
+    <body>
+        <h1>Hola Mundo</h1>
+        <h1>Hola , {{$name}}</h1>
+
+    </body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\View;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -13,8 +15,60 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+//default route
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// Route::get('/', function () {
+//     return view('vista1');
+// });
+
+// Route::get('/{var}', function ($var) {
+//     return view('vista1', ['name'=> $var]);
+// });
+
+if(View::exists('vista2'))
+{
+    Route::get('/', function () {
+        return view('vista2');
+    });
+}
+else{
+    Route::get('/', function () {
+        return "the needed view doesnt exist!";
+    });
+}
+//route
+// Route::get('/route1', function () {
+//     return "<h1> This is a test</h1>";
+// });
+
+//route with var
+Route::get('/route1/{name?}', function ($name = 'younes') {
+    return "<h1>Hola ".$name."</h1>";
+})->name("route01");
+
+//route with where
+Route::get('/user/{user}', function ($user) {
+    return "<h1>Hola ".$user."</h1>";
+})->where('user', '[0-9]+');//[A-Za-z]+
+
+//route with redirect function
+Route::get('/route2', function () {
+    return redirect()->route("route01");
+});
+
+//route with associative array
+Route::get('/array', function () {
+    //route with array
+    // $arr = ['lundi', 'mardi', 'mercredi'];
+    //route with associative array
+    $arr = [
+        'id'=> '12',
+        'desc'=> 'product 01'
+    ];
+    return $arr;
 });
 
 Route::middleware([
